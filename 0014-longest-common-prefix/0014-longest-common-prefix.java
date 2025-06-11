2265e41677b4de375e9e0018a7627b840eb33822
Abhinav Raj <137479529+abbinavv@users.noreply.github.com>
2025-06-11 23:46:18 +0530

    Time: 1 ms (64.25%), Space: 41.8 MB (43.13%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -6,12 +6,11 @@ class Solution {
         for (int i = 0; i < strs[0].length(); i++) {
             char ch = strs[0].charAt(i);
             for (int j = 1; j < strs.length; j++) {
-                // If index i is out of bounds or characters don't match
                 if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
                     return strs[0].substring(0, i);
                 }
             }
         }
-        return strs[0]; // Entire first string is the prefix
+        return strs[0]; 
     }
 }

aced8a311e5068b590d15757d2bfd93a3da1a40c
Abhinav Raj <abhinavraj00001@gmail.com>
2026-09-01 22:54:01 +0530

    Time: 563 ms (45.24%), Space: 282 MB (35.71%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.java
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.java
@@ -20,8 +20,6 @@ class Solution {
         int startR = 0;
         int startC = 0;
 
-        // litterIndex[r][c] tells us which litter this cell represents
-        // -1 means this cell is not litter
         int[][] litterIndex = new int[m][n];
 
         for (int[] row : litterIndex) {
@@ -30,7 +28,6 @@ class Solution {
 
         int litterCount = 0;
 
-        // Find starting position and assign an index to every litter
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
 
@@ -48,27 +45,17 @@ class Solution {
             }
         }
 
-        // No litter to collect
         if (litterCount == 0) {
             return 0;
         }
 
-        // Example: litterCount = 3
-        // 1 << 3 = 1000
-        // - 1   = 0111
+
         int allCollected = (1 << litterCount) - 1;
 
-        /*
-         * visited[r][c][energy][mask]
-         *
-         * mask tells us which litter has been collected.
-         */
         boolean[][][][] visited =
             new boolean[m][n][energy + 1][1 << litterCount];
 
         Queue<State> queue = new LinkedList<>();
-
-        // Starting state
         queue.offer(new State(startR, startC, energy, 0, 0));
         visited[startR][startC][energy][0] = true;
 
@@ -78,40 +65,26 @@ class Solution {
         while (!queue.isEmpty()) {
 
             State curr = queue.poll();
-
-            // We collected every litter
             if (curr.mask == allCollected) {
                 return curr.moves;
             }
 
-            // No energy -> cannot make another move
             if (curr.energy == 0) {
                 continue;
             }
 
-            // Try 4 directions
             for (int d = 0; d < 4; d++) {
 
                 int nr = curr.r + dr[d];
                 int nc = curr.c + dc[d];
-
-                // Outside grid
                 if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
                     continue;
                 }
-
-                // Obstacle
                 if (classroom[nr].charAt(nc) == 'X') {
                     continue;
                 }
-
-                // Moving costs 1 energy
                 int newEnergy = curr.energy - 1;
-
-                // Keep current litter mask
                 int newMask = curr.mask;
-
-                // If we stepped on litter
                 if (classroom[nr].charAt(nc) == 'L') {
 
                     int index = litterIndex[nr][nc];
@@ -119,12 +92,10 @@ class Solution {
                     newMask = newMask | (1 << index);
                 }
 
-                // Reset energy if we stepped on R
                 if (classroom[nr].charAt(nc) == 'R') {
                     newEnergy = energy;
                 }
 
-                // If this exact state hasn't been visited
                 if (!visited[nr][nc][newEnergy][newMask]) {
 
                     visited[nr][nc][newEnergy][newMask] = true;
@@ -141,8 +112,6 @@ class Solution {
                 }
             }
         }
-
-        // Impossible to collect all litter
         return -1;
     }
 }
